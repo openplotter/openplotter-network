66015085d0cd7ea28455ebcaec474e1e8a14b65e
e-sailing <e.minus.sailing@gmail.com>
2022-03-27 19:05:27 +0200

    fix

--- openplotterNetwork/Network/.openplotter/wpa_cli_script.sh
+++ openplotterNetwork/Network/.openplotter/wpa_cli_script.sh
@@ -12,7 +12,8 @@ function work () {
 	if [ "$wlanExists" ]; then
 		echo $wlan exists
 
-		wlanHandle="$(sudo nft --handle --numeric list chain inet filter INPUT | grep "$wlan" | cut -d'#' -f 2 | cut -d' ' -f 3)"
+		wlanHandle="$(sudo nft --handle --numeric list chain inet filter INPUT | grep usb0 | cut -d'#' -f 2 | cut -d' ' -f 3)"
+		wlan9 = "$(sudo nft list ruleset | grep wlan9)"
 		ssid="$(sudo wpa_cli status -i "$wlan" | grep "\bssid" | cut -d'=' -f 2)"
 		echo $wlan" handle:" $wlanHandle
 		echo "ssid:" $ssid
@@ -25,8 +26,11 @@ function work () {
 				if [ "$wlanHandle" ]
 				then
 					# text length > 0 -> blocked must be deleted
-					sudo nft delete rule inet filter INPUT handle "$wlanHandle"
-					echo delete rule $wlan
+					#sudo nft delete rule inet filter INPUT handle "$wlanHandle"
+					#echo delete rule $wlan
+					sudo nft delete chain inet filter INPUT
+					sudo nft add 'chain inet filter INPUT {type filter hook input priority filter; policy accept;}'
+					echo open input filter
 				else
 					echo no rule to delete					
 				fi
@@ -36,8 +40,23 @@ function work () {
 				if [ -z "$wlanHandle" ]
 				then
 					echo "public network not blocked must be added"
-					sudo nft add rule inet filter INPUT iifname $wlan drop
-					echo add rule $wlan
+					#sudo nft add rule inet filter INPUT iifname $wlan drop
+					#echo add rule $wlan
+					sudo nft delete chain inet filter INPUT
+					sudo nft add 'chain inet filter INPUT {type filter hook input priority filter; policy drop;}'
+					sudo nft add rule inet filter INPUT iifname "lo" accept
+					sudo nft add rule inet filter INPUT iifname "eth1" accept
+					sudo nft add rule inet filter INPUT iifname "usb0" accept
+					if [ "$wlan9" ]; then
+						sudo nft add rule inet filter INPUT iifname "eth0" accept
+						sudo nft add rule inet filter INPUT iifname "wlan9" accept
+					else
+						sudo nft add rule inet filter INPUT iifname "br0" accept
+					fi
+					sudo nft add rule inet filter INPUT ct state { established, related } accept
+					sudo nft add rule inet filter INPUT ct state invalid drop
+					sudo nft add rule inet filter INPUT ip protocol icmp accept
+					echo block wlan0 and wlan1 for outside
 				else
 					echo rule already exists					
 				fi
